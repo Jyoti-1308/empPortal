80987770007cd5eece347a8fdc91b6ac305ccbe1
Jyoti_Parashar2000 <parasharjyoti2000@gmail.com>
2023-12-28 20:01:37 +0530

    commit empportal2

--- empPortalServer.js
+++ empPortalServer.js
@@ -11,12 +11,12 @@ app.use(function (req, res, next) {
   next();
 });
 const cors = require('cors');
-const corsOptions = {
-    origin: 'http://localhost:3000',
-    credentials: true,            //access-control-allow-credentials:true
-    optionSuccessStatus: 200
-}
-app.use(cors(corsOptions));
+// const corsOptions = {
+//     origin: 'http://localhost:3000',
+//     credentials: true,            //access-control-allow-credentials:true
+//     optionSuccessStatus: 200
+// }
+// app.use(cors(corsOptions));
 var users = [
   {
     role: "ADMIN",
